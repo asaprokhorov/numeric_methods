b7c67cac032a1d6b9b5cf5994799acbfe76b5ff5
sanyokprokhor <oleksandrprohr@gmail.com>
2015-11-28 22:48:38 +0200

    Lab 6

---
+++ numeric_methods_lab6.py
@@ -0,0 +1,50 @@
+from matplotlib import pyplot as plt
+
+
+def f(x):
+    return 2 * x**2
+
+
+def p(x):
+    return -0.5
+
+
+def q(x):
+    return 3.0
+
+
+a = 1.0
+b = 1.3
+alpha0 = 1.0
+alpha1 = 2.0
+beta0 = 1.0
+beta1 = 0.0
+A = 0.6
+B = 1
+
+def solve(n):
+    xval = []
+    yval = []
+    c = []
+    d = []
+    h = (b - a) / n
+    c.append(alpha1 / (h * alpha0 - alpha1))
+    d.append(A * h / alpha1)
+    xval.append(a)
+    for i in range(1, n):
+        xval.append(a + i * h)
+        mi = ((h ** 2) * q(xval[i]) - 2) / (1 + h * p(xval[i]) / 2)
+        ki = (1 - h * p(xval[i]) / 2) / (1 + h * p(xval[i]) / 2)
+        Fi = f(xval[i]) / (1 + p(xval[i]) / 2)
+        c.append(1 / (mi - ki * c[i - 1]))
+        d.append((h ** 2) * Fi - ki * c[i - 1] * d[i - 1])
+    xval.append(b)
+    yval.append((B * h + beta1 * c[n - 1] * d[n - 1]) / (beta0 * h + beta1 * (c[n - 1] + 1)))
+    for i in range(n):
+        yval.append(c[n - 1 - i] * (d[n - 1 - i] - yval[i]))
+    yval.reverse()
+    return xval, yval
+
+xcoords, ycoords = solve(100)
+plt.plot(xcoords, ycoords)
+plt.show()
